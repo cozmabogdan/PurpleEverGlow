b25b27755f40eda56a98457abc2eb70ba147cf41
Cozma BOgdan <bogdan>
2021-01-30 12:31:48 +0100

    Add edit/delete function and contact functionality

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const fs = require("fs");
 const path = require("path");
 const multer = require("multer");
 const base64 = require("node-base64-image");
+const nodemailer = require("nodemailer");
+const { getMaxListeners } = require("process");
 require("dotenv/config");
 
 //author variable
@@ -83,6 +85,24 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage: storage});
 
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS,
+    }
+});
+
+//verify connection config for mailer
+transporter.verify(function(err, succes){
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("Server is ready to take our messages");
+    }
+});
+
 app.get("/", function(req, res){
     Post.find({}, function(err, posts){
         res.render("home", {
@@ -145,24 +165,23 @@ app.get("/login", function(req, res){
     res.render("login");
 });
 
-app.get("/modify", function(req, res){
-    Post.find({}, function(err, posts){
-        res.render("modify", {
-            posts: posts
-        });
-    })
-});
-
 app.get("/edit/:postId", function(req, res){
     const articleId = req.params.postId;
     requestedPostId.push(articleId);
-    console.log(articleId);
     Post.findOne({_id: requestedPostId}, function(err, post){
         let newLine = post.content.replace(/(\r\n)/gm, '<br><br>');
         res.render("edit", {
             title: post.title,
             content: post.content
         });
+    });
+});
+
+//delete one document from database
+app.get("/delete/:postId", function(req, res){
+    const requestedId = req.params.postId;
+    Post.deleteOne({_id: requestedId}, function(err, post){
+        res.redirect("/admin");
     });
 });
 
@@ -216,14 +235,40 @@ app.post("/register", function(req, res){
 });
 
 //replace one document in the DB
-app.post("/edit", function(req, res){
-    Post.replaceOne({_id: requestedPostId}, {title: req.body.editTitle, content: req.body.editBody}, function(err, post){
-        console.log(req.body.title);
+app.post("/edit", upload.single("image"), function(req, res){
+    Post.replaceOne({_id: requestedPostId}, {
+        title: req.body.editTitle,
+        content: req.body.editBody,
+        author: author,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + "/uploads/" + req.file.filename)),
+            contentType: "image/jpg"
+        }
+    }, function(err, post){
         requestedPostId.splice(0, 1);
-        res.redirect("modify");
+        res.redirect("admin");
     });
 });
 
+app.post("/contact", function(req, res){
+    const mail = {
+        from: req.body.email,
+        to: process.env.EMAIL,
+        subject: req.body.subject,
+        text: req.body.content
+    };
+    
+    transporter.sendMail(mail, function(err, data){
+        if(err){
+            console.log(err);
+            res.status(500).send("Something went wrong.");
+        } else {
+            res.status(200).send("Email successfully sent!");
+        }
+    });
+    
+    res.redirect("/");
+})
 
 app.listen(3000, function(){
     console.log("Server connected!")
